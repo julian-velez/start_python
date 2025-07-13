9c228cf6a45f912b96e2c59d7b5e947b49bdd6ab
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-07-13 15:52:51 -0500

    start_python
    # Se define una variable llamada 'texto' que contiene una cadena de texto
    texto = "hola, julian velez G."
    
    # Se usa el método 'startswith' para verificar si la cadena comienza con la palabra "hola"
    # Este método devuelve True si 'texto' inicia exactamente con "hola", de lo contrario devuelve False
    verificar = texto.startswith("hola")
    
    # Se imprime el resultado de la verificación en la consola
    print(verificar)  # En este caso, imprime True porque la cadena sí comienza con "hola"

---
+++ start/ejemplo.py
@@ -0,0 +1,9 @@
+# Se define una variable llamada 'texto' que contiene una cadena de texto
+texto = "hola, julian velez G."
+
+# Se usa el método 'startswith' para verificar si la cadena comienza con la palabra "hola"
+# Este método devuelve True si 'texto' inicia exactamente con "hola", de lo contrario devuelve False
+verificar = texto.startswith("hola")
+
+# Se imprime el resultado de la verificación en la consola
+print(verificar)  # En este caso, imprime True porque la cadena sí comienza con "hola"
